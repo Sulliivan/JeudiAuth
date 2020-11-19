1de2a2f2607c5fd1006ef16e170fef1b6fdf56b1
Sulliivan <qraks-@hotmail.fr>
2020-11-19 14:27:54 +0100

    Initial commit

---
+++ JeudiAuth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JeudiAuth.Startup))]
+namespace JeudiAuth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
